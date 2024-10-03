365ce94177e752255dac719468451c5126103fc2
krina <krinabhalani@gmail.com>
2024-10-03 11:23:05 +0530

    Create cart.js

---
+++ cart.js
@@ -0,0 +1,5 @@
+<!------------------------- Find this function "updateQuantity" ------------------------------>
+<!------------------------ in this function find this line "this.updateLiveRegions(line, message);" and add below code after this line --------------------------->
+
+          updateCheckoutButton();
+          updateCartListeners();
